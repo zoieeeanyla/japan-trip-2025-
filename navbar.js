e7519db1a75c252c95799a78a8d8175c513468be
zoieeeanyla <Zoie608@gmail.com>
2025-05-13 18:37:15 -0400

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,14 @@
+document.getElementById("navbar").innerHTML = `
+  <div class="navbar">
+    <a href="index.html">Home</a>
+    <a href="day1.html">Day 1</a>
+    <a href="day2.html">Day 2</a>
+    <a href="day3.html">Day 3</a>
+    <a href="day4.html">Day 4</a>
+    <a href="day5.html">Day 5</a>
+    <a href="day6.html">Day 6</a>
+    <a href="day7.html">Day 7</a>
+    <a href="day8.html">Day 8</a>
+    <a href="budget.html">Budget</a>
+  </div>
+`;
